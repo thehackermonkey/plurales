fce099eb8f66e9cb51636ee2fabc91741b578342
Hans Ramírez <hans@thehackermonkey.com>
2022-05-17 01:40:43 -0500

    first commit

---
+++ src/plurales.js
@@ -0,0 +1,43 @@
+function plurales(str) {
+  const sPluralVowels = ["a", "e", "i", "o", "u", "é"];
+  const pluralConsonants = [
+    "q",
+    "w",
+    "r",
+    "t",
+    "y",
+    "p",
+    "d",
+    "f",
+    "g",
+    "h",
+    "j",
+    "k",
+    "l",
+    "c",
+    "v",
+    "b",
+    "n",
+    "m",
+    "é",
+  ];
+  const lastChar = str.toLowerCase().charAt(str.length - 1);
+
+  var validString = new RegExp("[A-zÀ-ú]$");
+
+  if (validString.test(str)) {
+    if (lastChar === "x" || lastChar === "s") {
+      return str.toLowerCase();
+    } else if (sPluralVowels.includes(lastChar)) {
+      return str.toLowerCase() + "s";
+    } else if (pluralConsonants.includes(lastChar)) {
+      return str.toLowerCase() + "es";
+    } else if (lastChar === "z") {
+      return str.toLowerCase().replace(/.$/, "ces");
+    }
+  } else {
+    throw new Error("Invalid string");
+  }
+}
+
+module.exports = plurales;
